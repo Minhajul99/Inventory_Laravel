efc1fca3f856aed94bf05535016d7e1e55287483
Minhajul abedin <minhajul15-11140@diu.edu.bd>
2022-09-04 14:08:31 +0600

    first commit

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    public function destroy(Request $request)
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/');
+    }
+
+    public function Profile(){
+
+        $id =Auth::user()->id;
+        $adminData = User::find($id);
+        return view('admin.admin_profile', compact('adminData'));
+    }
+}
---
+++ resources/views/components/application-logo.blade.php
@@ -0,0 +1,1 @@
+<img src="{{asset('logo/owl2.jpg')}}" width="80px" alt="">
